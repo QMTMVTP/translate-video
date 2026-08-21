bdc17f4178241e6a304f9eebf17405588e23bf60
QMTMVTP <gianinhitk22@gmail.com>
2026-08-21 21:15:59 +0700

    Fix undefined spawn for FFmpeg

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const fs = require('fs-extra');
 const multer = require('multer');
-const { execFile } = require('child_process');
+const { execFile, spawn } = require('child_process');
 const ffmpegPath = require('ffmpeg-static');
 const { v4: uuidv4 } = require('uuid');
 
